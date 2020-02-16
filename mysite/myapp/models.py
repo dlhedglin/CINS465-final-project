6915ba5078c61c20d220b756ee5d04fa2734f71d
Devan Hedglin <dlhedglin@mail.csuchico.edu>
2020-02-16 03:31:34 -0800

    Added artist and picture models

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    insta_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{0} {1} {2}".format(self.first_name, self.last_name, self.insta_name)
+
+class Picture(models.Model):
+    picture_url = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.picture_url
---
+++ mysite/myapp/scrape.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+BASE_URL = 'https://www.instagram.com/'
+
+# modified from https://github.com/rarcega/instagram-scraper/
+def get_shared_data(username=''):
+        """Fetches the user's metadata."""
+        resp = requests.get(BASE_URL + username).text
+        if resp is not None and '_sharedData' in resp:
+            try:
+                shared_data = resp.split("window._sharedData = ")[1].split(";</script>")[0]
+                return json.loads(shared_data)
+            except (TypeError, KeyError, IndexError):
+                pass
+def getUserId(username):
+    return get_shared_data(username)['entry_data']['ProfilePage'][0]['graphql']['user']['id']
+
+def extractLinks(shared_data):
+    links = []
+    for edge in shared_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']:
+        link = edge['node']['display_url']
+        links.append(link)
+    return links
